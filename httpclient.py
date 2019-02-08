6e6fd5a35fed9560c767f8cbd0ba3596d3baa40d
Pavlo Malynin <pavlo@malynin.ca>
2019-02-08 00:06:53 -0700

    Asn 2

--- httpclient.py
+++ httpclient.py
@@ -21,6 +21,7 @@
 import sys
 import socket
 import re
+from collections import defaultdict
 # you may use urllib to encode data appropriately
 import urllib.parse
 
@@ -36,25 +37,41 @@ class HTTPClient(object):
     #def get_host_port(self,url):
 
     def connect(self, host, port):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
-        return None
-
-    def get_code(self, data):
-        return None
-
-    def get_headers(self,data):
-        return None
-
-    def get_body(self, data):
+        self.socket = socket.create_connection((host, port))
         return None
     
-    def sendall(self, data):
-        self.socket.sendall(data.encode('utf-8'))
-        
     def close(self):
         self.socket.close()
+    
+    def setup(self, url):
+        u = urllib.parse.urlparse(url)
+        assert u.hostname is not None
+        host = u.hostname
+        port = u.port if u.port is not None else 80
+        self.host = u.netloc
+        self.port = port
+        self.path = u.path if u.path else "/"
+        self.connect(host, port)
 
+    def parse_status(self):
+        self.ver, self.code, self.text = self.data[0].split(" ", maxsplit=2)
+        self.code = int(self.code)
+        # print(self.data)
+    
+    def parse_headers(self):
+        self.headers = defaultdict(list)
+        for i in range(1, len(self.data)):
+            if self.data[i] == '\r':
+                self.endh = i + 1
+                break
+            h, v = self.data[i].split(":", maxsplit=1)
+            self.headers[h].append(v)
+
+
+
+    def write_header(self, header):
+        # print(f"> {header[0]}: {header[1]}")
+        self.socket.sendall(f"{header[0]}: {header[1]}\r\n".encode("utf-8"))
     # read everything from the socket
     def recvall(self, sock):
         buffer = bytearray()
@@ -68,15 +85,46 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        self.setup(url)
+        self.socket.sendall(f"GET {self.path} HTTP/1.1\r\n".encode("utf-8"))
+        self.write_header(("Host", self.host))
+        self.write_header(('Accept-Encoding', "identity"))
+        self.write_header(('Accept-Charset', "utf-8"))
+        self.write_header(("User-Agent", "curl/7.54.0"))
+        self.write_header(("Accept", "*/*"))
+        self.write_header(("Connection", "close"))
+        self.socket.sendall(b"\r\n")
+        self.socket.shutdown(socket.SHUT_WR)
+        
+        self.data = self.recvall(self.socket).split("\n")
+        self.parse_status()
+        self.parse_headers()
+        self.close()
+        return HTTPResponse(self.code, '\n'.join(self.data[self.endh:]))
 
-    def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
 
+    def POST(self, url, args=dict()):
+        senddata = urllib.parse.urlencode(args).encode("utf-8")
+
+        self.setup(url)
+        self.socket.sendall(f"POST {self.path} HTTP/1.1\r\n".encode("utf-8"))
+        self.write_header(("Host", self.host))
+        self.write_header(("Content-Type", "application/x-www-form-urlencoded"))
+        self.write_header(("Content-Length", str(len(senddata))))
+        self.write_header(('Accept-Encoding', "identity"))
+        self.write_header(('Accept-Charset', "utf-8"))
+        self.write_header(("User-Agent", "curl/7.54.0"))
+        self.write_header(("Accept", "*/*"))
+        self.write_header(("Connection", "close"))
+        self.socket.sendall(b"\r\n")
+        self.socket.sendall(senddata)
+        self.socket.shutdown(socket.SHUT_WR)
+        
+        self.data = self.recvall(self.socket).split("\n")
+        self.parse_status()
+        self.parse_headers()
+        self.close()
+        return HTTPResponse(self.code, '\n'.join(self.data[self.endh:]))
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
             return self.POST( url, args )
